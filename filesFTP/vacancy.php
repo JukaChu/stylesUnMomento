f86986b8615d0e85ea697407c1d6c8d4600f59ac
JukaChu <olexiy.yankevich@gmail.com>
2022-01-15 14:17:50 +0200

    from home

--- filesFTP/vacancy.php
+++ filesFTP/vacancy.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 $vakansii = dbQuery("SELECT id,url,name,preview, info, info2
 FROM content WHERE parentid = 2987 AND ispublish = 1 ORDER BY showorder"); ?>
+
 <main>
     <div class="container">
         <h1 class="main-title">
@@ -12,87 +13,68 @@ FROM content WHERE parentid = 2987 AND ispublish = 1 ORDER BY showorder"); ?>
             <div class="cards-section__container">
                 <div class="cards-list swiper-container">
                     <div class="swiper-wrapper">
-                        <a href="" class="single-card swiper-slide green">
-                            <div class="img">
-                                <img class="lazyload" data-src="" alt="">
-                            </div>
-                            <div class="text">
-                                <span class="tag">Вакансії</span>
-                                <p></p>
-                                <div class="bottom">
-                                    <div class="btn btn--blue">Детальніше</div>
+
+                        <?php foreach ($vakansii as $key=>$r) { ?>
+                            <div class="card-obt-cont swiper-slide">
+                                <div class="single-card  green">
+                                    <div class="img">
+                                        <img class="lazyload" data-src="<?php echo _TEMPL . 'build/img/vk-' ?><?php echo $key + 1  .'.png' ?>" alt="">
+                                    </div>
+                                    <div class="text">
+                                        <span class="tag">Вакансії</span>
+                                        <p><?= $r['name'] ?></p>
+                                        <div class="bottom">
+                                            <small><?php echo htmlspecialchars_decode($r['preview'], ENT_QUOTES); ?></small>
+                                            <div class="btn btn--blue">Детальніше</div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="card-obt__text">
+                                    <?php echo htmlspecialchars_decode($r['info'], ENT_QUOTES); ?>
+                                    <?php echo htmlspecialchars_decode($r['info2'], ENT_QUOTES); ?>
                                 </div>
 
                             </div>
-                        </a>
+
+                        <?php } ?>
+
                     </div>
                 </div>
+                <div class="slider-buttons">
+                    <div class="btn-slides btn-slides--prev">
+                        <svg width="28" height="88" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M26.5 2l-24 42 24 42" stroke-width="3" stroke-linecap="round"/>
+                        </svg>
+                    </div>
+                    <div class="btn-slides btn-slides--next">
+                        <svg width="28" height="88" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M26.5 2l-24 42 24 42" stroke-width="3" stroke-linecap="round"/>
+                        </svg>
+                    </div>
+
+                </div>
+                <div class="dots"></div>
             </div>
+        </div>
+        <div class="desk-socials">
+            <a target="_blank" href="https://m.me/momento.un?ref=UNMOMENTO" class="desk-soc">
+                <img src="<?php echo _TEMPL.'build/img/ds-ms.svg'?>" alt="">
+            </a>
+            <a target="_blank" href="viber://pa?chatURI=unmomentobot&context=ml1" class="desk-soc">
+                <img src="<?php echo _TEMPL.'build/img/ds-vb.svg'?>" alt="">
+            </a>
+            <a target="_blank" href="https://t.me/unmomento_bot" class="desk-soc">
+                <img src="<?php echo _TEMPL.'build/img/ds-tg.svg'?>" alt="">
+            </a>
+            <a onclick="gtag('event', 'Click', {'event_category': 'Order'});" href="https://play.google.com/store/apps/details?id=ru.agbis.Unmomento" class="desk-soc">
+                <img src="<?php echo _TEMPL.'build/img/ds-pm.svg'?>" alt="">
+            </a>
+            <a onclick="gtag('event', 'Click', {'event_category': 'Order'});" href="https://apps.apple.com/ua/app/unmomento/id1491483430?l=ru" class="desk-soc">
+                <img src="<?php echo _TEMPL.'build/img/ds-as.svg'?>" alt="">
+            </a>
         </div>
     </section>
+
 </main>
-<div class="main_content block_margin clear_after">
-        <?php include("company-menu.php"); ?>   
-        <div class="block_vacancies">
-            <div class="vacancies-c">
-            <?php foreach($vakansii  as $r) {?>
-                <div class="one-vacancy-tab">
-                    <div class="vacancy-tab-inner">
-                        <div class="vacancy-header">
-                            <div class="vacancy-info">
-                                <span><b><?=$r['name']?></b></span>
-                                <span><?php echo htmlspecialchars_decode($r['preview'],ENT_QUOTES);?></span>
-                            </div>
-                            <div class="more-btn showVacancy">Детальніше</div>
-                        </div>
 
-                        <div class="vacancy-hidden">
-                             <div class="vacancy-desc">
-                                <div class="vacancy-title">Вимоги:</div>
-                                <div class="vacancy-text">
-                                    <?php echo htmlspecialchars_decode($r['info'],ENT_QUOTES);?>
-                                </div>
-                                <div class="vacancy-title">Обов'язки: </div>
-                                <div class="vacancy-text">
-                                    <?php echo htmlspecialchars_decode($r['info2'],ENT_QUOTES);?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <?php }?>
-            </div>
-        </div>
-    <div class="container_catalog clear_after">
-        <div class="vakansii-header-text">
-            <p>&nbsp;</p>
-
-            <p>Компанія UNMOMENTO пропонує ідеальний варіант працевлаштування для тих, хто хоче працювати в крупній компанії, яка інтенсивно розвивається!</p>
-
-            <div class="margin-20">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span lang="uk-UA">По-перше, офіційне ОФОРМЛЕННЯ!</span><br>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span lang="uk-UA">По-друге, НАВЧАННЯ всіх професій за рахунок компанії!</span><br>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span lang="uk-UA">Також, оплачувана відпустка та лікарняний!</span><br>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span lang="uk-UA">І звичайно, СТАБІЛЬНА ЗАРОБІТНА ПЛАТА: ставка + премії</span></div>
-
-            <p><span lang="uk-UA">А ще, у нас:</span>:</p>
-
-            <div class="margin-20"><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</strong><span lang="uk-UA">Дружній колектив!</span><br>
-            <strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</strong><span lang="uk-UA">Гнучкий графік роботи!</span><br>
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <span lang="uk-UA">А при бажанні, тебе чекає кар'єрний ріст!</span></div>
-
-            <p>&nbsp;</p>
-
-            <p style="margin-bottom: 0cm; line-height: 100%"><span lang="uk-UA">З нетерпінням чекаємо тебе, колега;)</p>
-
-            <p>&nbsp;</p>
-            </div>
-        </div>
-        <div class="block_phone">
-        Звертайтесь за телефоном: <span> 
-        <a onclick="gtag('event', 'Click', {'event_category': 'Order'});" href="tel:+380955015638">095 501 56 38</a>
-        <br>
-        Контактна особа: </span> HR-manager Альона
-        </div>
-    </div>
-</div>
