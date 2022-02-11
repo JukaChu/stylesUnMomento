e3d382b0f571317faee00934e978100b9927978f
JukaChu <olexiy.yankevich@gmail.com>
2022-02-11 10:00:47 +0200

    from work

--- filesFTP/RU-version/price.php
+++ filesFTP/RU-version/price.php
@@ -169,6 +169,31 @@
 
         </div>
     </section>
-    <?php include 'desk-buttons-nav.php'; ?>
+    <section class="desktop-buttons">
+        <div class="container">
+            <div class="desk-buttons__list">
+                <a href="/uslugi/distantsionnyy-zakaz.html" class="desk-btn cour-non">Вызов курьера</a>
+                <div class="desk-socials">
+                    <a target="_blank" href="https://m.me/momento.un?ref=UNMOMENTO" class="desk-soc">
+                        <img src="<?php echo _TEMPL.'build/img/ds-ms.svg'?>" alt="">
+                    </a>
+                    <a target="_blank" href="viber://pa?chatURI=unmomentobot&context=ml1" class="desk-soc">
+                        <img src="<?php echo _TEMPL.'build/img/ds-vb.svg'?>" alt="">
+                    </a>
+                    <a target="_blank" href="https://t.me/unmomento_bot" class="desk-soc">
+                        <img src="<?php echo _TEMPL.'build/img/ds-tg.svg'?>" alt="">
+                    </a>
+                    <a onclick="gtag('event', 'Click', {'event_category': 'Order'});" href="https://play.google.com/store/apps/details?id=ru.agbis.Unmomento" class="desk-soc">
+                        <img src="<?php echo _TEMPL.'build/img/ds-pm.svg'?>" alt="">
+                    </a>
+                    <a onclick="gtag('event', 'Click', {'event_category': 'Order'});" href="https://apps.apple.com/ua/app/unmomento/id1491483430?l=ru" class="desk-soc">
+                        <img src="<?php echo _TEMPL.'build/img/ds-as.svg'?>" alt="">
+                    </a>
+                </div>
+                <a href="/o-kompanii/kontakty.html" class="desk-btn">Пункты приема</a>
+            </div>
+
+        </div>
+    </section>
 </main>
 
